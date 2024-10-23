de9c2e7eb94cc47c54d1ff67de17ce1b650b19c0
mizansanzi <pleasemailmizan@gmail.com>
2024-10-24 01:16:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './components/blogs/blogs'
+import Header from './components/header/header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-red-600 font-bold'>React Know<span className='text-lime-600'>ledge</span> Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
